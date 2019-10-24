7f414796f318fc6250bd2c309f3a7bd5b0202ba1
Luca Del Re <delreluca@googlemail.com>
2019-10-24 22:18:44 +0200

    Add config for fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+# ensure consistent locale
+set -xU LC_ALL en_US.UTF-8
+set -xU LANG en_US.UTF-8
+
+# add Stack directory for local binaries to path
+set -U fish_user_paths (stack path --local-bin)
+
+# disable .NET Core telemetry
+set -xU DOTNET_CLI_TELEMETRY_OPTOUT 1
+
+# MacPorts Installer addition on 2019-07-20_at_14:42:02: adding an appropriate PATH variable for use with MacPorts.
+set -U fish_user_paths /opt/local/bin /opt/local/sbin
